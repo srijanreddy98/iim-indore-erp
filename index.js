cb56a2f559f1d316fd8eb4e9fcb563ff95f9d597
srijanreddy98 <srijanreddy98@gmail.com>
2017-12-19 07:39:57 +0530

    firstCommit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// const express = require('express');
+const express = require('express');
 // const mongoose = require('mongoose');
 // const cookieSession = require('cookie-session');
 // const passport = require('passport');
 // const {keys} = require('./keys/keys');
 // require('./services/passport');
-// const {routes} = require('./routes/authRoutes');
+const {routes} = require('./routes/authRoutes');
 console.log('hey');
 // const {User} = require('./models/models');
 //
@@ -19,6 +19,6 @@ console.log('hey');
 // );
 // app.use(passport.initialize());
 // app.use(passport.session());
-// routes(app);
-// const PORT = process.env.POST || 3000;
-// app.listen(PORT, () => console.log(`Server is up on port:${PORT}`));
+routes(app);
+const PORT = process.env.POST || 3000;
+app.listen(PORT, () => console.log(`Server is up on port:${PORT}`));
