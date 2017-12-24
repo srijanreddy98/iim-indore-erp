f6914034348f48ad9e4f6258c0a9a77917460666
srijanreddy98 <srijanreddy98@gmail.com>
2017-12-25 00:15:57 +0530

    v1.0.0@alpha1

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,6 @@ passport.use(
     clientID: keys.clientID,
     clientSecret: keys.client_secret,
     callbackURL: '/auth/google/callback',
-    proxy: true
   },
   (accessToken,refreshToken, profile, done) => {
     User.findOne({googleId: profile.id}).then(
