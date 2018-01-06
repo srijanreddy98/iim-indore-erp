d4d17b1a7f47d88bd34c2787d2755ea1beb1640b
srijanreddy98 <srijanreddy98@gmail.com>
2018-01-06 10:36:25 +0530

    timetable added v0.1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist'));
 // app.use(express.static('dist'));
 app.use(
   cookieSession({
@@ -25,9 +25,9 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-// app.get('/client/*', function (req, res) {
-// res.sendFile(path.join(__dirname+'/dist','index.html'));
-// });
+app.get('/client/*', function (req, res) {
+res.sendFile(path.join(__dirname+'/dist','index.html'));
+});
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 // var err = new Error('Not Found It');
